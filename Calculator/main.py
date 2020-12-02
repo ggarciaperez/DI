08e4c69730eb6d354ebfa4f11a2e72c55f7fbd95
Guillermo <ggarciaperez@danielcastelao.org>
2020-12-02 09:26:25 -0800

    VM breaking

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import gi
+gi.require_version("Gtk","3.0")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from gi.repository import Gtk
+
+class panel (Gtk.Grid):
+    def __init__(self):
+        Gtk.Grid.__init__(self)
+        boton1=Gtk.Button(label="1")
+        boton2 = Gtk.Button(label="2")
+        boton3 = Gtk.Button(label="3")
+        boton4 = Gtk.Button(label="4")
+        boton5 = Gtk.Button(label="5")
+        boton6 = Gtk.Button(label="6")
+        boton7 = Gtk.Button(label="7")
+        boton8 = Gtk.Button(label="8")
+        boton9 = Gtk.Button(label="9")
+        boton0 = Gtk.Button(label="0")
+        botonPunto = Gtk.Button(label=".")
+        botonDiv = Gtk.Button(label="/")
+        botonMult = Gtk.Button(label="*")
+        botonMen = Gtk.Button(label="-")
+        botonMas = Gtk.Button(label="+")
+        botonIgual = Gtk.Button(label="=")
+        botonCe = Gtk.Button(label="CE")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+class Caja(Gtk.Window):
+    def __init__(self):
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    Caja()
+    Gtk.main()
